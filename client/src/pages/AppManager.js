321caea28f0327740ea1ee5157ec6e56a67c4c86
IT22296764 <it22296764@my.sliit.lk>
2024-05-12 19:43:45 +0530

    bkup

--- client/src/pages/AppManager.js
+++ client/src/pages/AppManager.js
@@ -11,8 +11,7 @@ const AppManager = () => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const formattedDate = date.toISOString().split('T')[0];
-   
-    return formattedDate; 
+    return formattedDate;
   };
 
   useEffect(() => {
@@ -29,41 +28,26 @@ const AppManager = () => {
     fetchAppointmentsData();
   }, []);
 
-  // Handle Search
   const handleSearch = async () => {
-    const results = appointments.filter(item => item.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    const results = appointments.filter((item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
     setAppointments(results);
   };
 
-  const handleApprove = async (id) => {
+  const updateAppointmentStatus = async (id, status) => {
     try {
-      const response = await axios.post(`http://localhost:3002/api/appManager/${id}/approve`);
+      const response = await axios.post(`/api/appointment/${id}/${status}`);
       const updatedAppointment = response.data.appointment;
       setAppointments((prevAppointments) =>
         prevAppointments.map((appointment) =>
           appointment._id === updatedAppointment._id ? updatedAppointment : appointment
         )
       );
-      message.success('Appointment approved successfully');
+      message.success(`Appointment ${status} successfully`);
     } catch (error) {
-      console.error('Error approving appointment:', error);
-      message.error('Failed to approve appointment. Please try again later.');
-    }
-  };
-
-  const handleReject = async (id) => {
-    try {
-      const response = await axios.post(`http://localhost:3002/api/appManager/${id}/reject`);
-      const updatedAppointment = response.data.appointment;
-      setAppointments((prevAppointments) =>
-        prevAppointments.map((appointment) =>
-          appointment._id === updatedAppointment._id ? updatedAppointment : appointment
-        )
-      );
-      message.success('Appointment rejected successfully');
-    } catch (error) {
-      console.error('Error rejecting appointment:', error);
-      message.error('Failed to reject appointment. Please try again later.');
+      console.error(`Error ${status} appointment:`, error);
+      message.error(`Failed to ${status} appointment. Please try again later.`);
     }
   };
 
@@ -77,6 +61,11 @@ const AppManager = () => {
       title: "Phone",
       dataIndex: "phoneNumber",
       key: "phoneNumber",
+    },
+    {
+      title: "Service",
+      dataIndex: "service",
+      key: "service",
     },
     {
       title: "Doctor",
@@ -99,37 +88,51 @@ const AppManager = () => {
       key: 'action',
       render: (text, record) => (
         <span>
-          <Button type="primary" onClick={() => handleApprove(record._id)} disabled={record.status !== 'pending'}>
+          <Button
+            type="primary"
+            onClick={() => updateAppointmentStatus(record._id, 'approve')}
+            disabled={record.status !== 'pending'}
+          >
             Approve
           </Button>
-          <Button onClick={() => handleReject(record._id)} disabled={record.status !== 'pending'}>
+          <Button
+            onClick={() => updateAppointmentStatus(record._id, 'reject')}
+            disabled={record.status !== 'pending'}
+          >
             Reject
           </Button>
         </span>
       ),
     },
-  ]; 
+  ];
 
   return (
     <Layout>
       <h1 className='page-title'>Appointments</h1>
       <div className='search'>
-        <input 
+        <input
           className='searchInput'
           placeholder=' Search by Name'
           prefix={<SearchOutlined />}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          style={{width:"250px",height: "30px",border:"none", boxShadow:"none", borderRadius:'50px' , marginLeft: '15px'}}
+          style={{
+            width: "250px",
+            height: "30px",
+            border: "none",
+            boxShadow: "none",
+            borderRadius: '50px',
+            marginLeft: '15px'
+          }}
         />
         <SearchOutlined
-          style={{ fontSize: "20px", cursor: "pointer" , padding:"2.5px"}}
+          style={{ fontSize: "20px", cursor: "pointer", padding: "2.5px" }}
           onClick={handleSearch}
         />
       </div>
-      <Table 
-        columns={columns} 
-        dataSource={appointments.map(appointment => ({ ...appointment, key: appointment._id }))} 
+      <Table
+        columns={columns}
+        dataSource={appointments.map((appointment) => ({ ...appointment, key: appointment._id }))}
       />
     </Layout>
   );
